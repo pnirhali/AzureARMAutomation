4e9893b38a2cc6825f6bb99b29a3b228bb9a5454
Piyush <piyush.dhimate@rapidcircle.com>
2020-01-11 14:31:53 +1100

    Parameterized deploy.ps1 script

---
+++ Client.ps1
@@ -0,0 +1,7 @@
+$ScriptPath = Split-Path $MyInvocation.InvocationName
+Write-Output "Current script folder path $ScriptPath"
+$args = @()
+$args += ("-templateName", "VirtualNetwork")
+$cmd = "$ScriptPath\Deploy.ps1"
+Write-Output "file name: $cmd" 
+ Invoke-Expression "$cmd $args"
--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,42 @@
+#Passing template name in order to deploy selected template
+param ($templateName = "SqlServer")
+$templatePath = ""
+$templatePath = "Templates\$templateName.json"
+write-Output "selected Template path:$templatePath"
+
+#create template parameter object 
+$TemplateObject = @{ }
+switch ($templateName) {
+    "SqlServer" {
+        $SqlServerTemplateObject = @{
+            "administratorLogin"         = "admin1";
+            "administratorLoginPassword" = "zaq1ZAQ!";
+            "databaseName"               = "poonamdb";
+            "tier"                       = "Basic";
+            "skuName"                    = "Basic";
+            "location"                   = "Australia East";
+            "serverName"                 = "poonamserver";
+        }
+        $TemplateObject.SqlServerParams = $SqlServerTemplateObject
+        write-Output "Created Sql server param object"
+
+    }
+    "VirtualNetwork" {
+        $VirtualNetworkTemplateObject = @{ 
+            "name"                 = "resource21-Vnet";
+            "location"             = "North Europe";
+            "addressPrefix"        = "10.1.0.0/16";
+            "subnetName"           = "default";
+            "subnetAddressPrefix"  = "10.1.0.0/24";
+            "enableDdosProtection" = $false;
+        }
+        $TemplateObject.virtualNetworkParams =$VirtualNetworkTemplateObject
+        write-Output "Created Virtual Network param object"
+    }
+    Default { }
+}
+Write-Output "Created main template object for deployment cmd "$TemplateObject
+
 # connect to Azure
 Write-Output "Connecting to subscription"
 Connect-AzAccount  -Subscription "Free Trial"
@@ -8,22 +47,12 @@ New-AzResourceGroup -Name PoonamRG1 -Location "Australia East"
 
 #Deploy template
 Write-Output "creating template parameter object"
-#1.create template parameter oject
-$TemplateObject = @{
-    "administratorLogin"         = "admin1";
-    "administratorLoginPassword" = "zaq1ZAQ!";
-    "databaseName"               = "poonamdb";
-    "tier"                       = "Basic";
-    "skuName"                    = "Basic";
-    "location"                   = "Australia East";
-    "serverName"                 = "poonamserver";
-}
 
 #2.Deployment started
 Write-Output "Deployment started"
 New-AzResourceGroupDeployment -Name "RgDeployment" `
     -ResourceGroupName "PoonamRG1" `
-    -TemplateParameterObject $TemplateObject `
-    -TemplateFile "C:\Stuff\Poonam\AzureARM\Templates\Template.json"
+    -TemplateParameterObject  $TemplateObject  `
+    -TemplateFile $templatePath
 
    
