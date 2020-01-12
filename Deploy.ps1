e89efbe42a382cafad1837912c90d4701aac38ec
Poonam <Poonamdhimate@gmail.com>
2020-01-13 10:41:49 +1100

    Parameterized Subscription name and resource group name:Default subscription name will be "free trial" and if client doesn't send any resource group name then it will create RG with Poonam-randomChars

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,5 +1,20 @@
 #Passing template name in order to deploy selected template
-param ($templateName = "SqlServer")
+param ($SubscriptionName, $templateName, $resourceGroupName)
+
+# errorActionPreference will not execute further code if system gets any error.
+$ErrorActionPreference = "STOP"
+
+
+# local functions start ======================================
+function  Get-RandomChars {
+    param (
+        [int] $length = 5 
+    )
+    $randomChars = -join ((65..90) + ( 97..122) | Get-Random -Count $length | % { [char]$_ })
+   return $randomChars
+}
+
+# local functions end ======================================
 $templatePath = ""
 $templatePath = "Templates\$templateName.json"
 write-Output "selected Template path:$templatePath"
@@ -30,20 +45,29 @@ switch ($templateName) {
             "subnetAddressPrefix"  = "10.1.0.0/24";
             "enableDdosProtection" = $false;
         }
-        $TemplateObject.virtualNetworkParams =$VirtualNetworkTemplateObject
+        $TemplateObject.virtualNetworkParams = $VirtualNetworkTemplateObject
         write-Output "Created Virtual Network param object"
     }
     Default { }
 }
 Write-Output "Created main template object for deployment cmd "$TemplateObject
 
+#generate random resource group name if client doesn't send 
+if (!$resourceGroupName) {
+    $resourceGroupName ="Poonam-" + (Get-RandomChars -length 5)
+}
+
+
+# set  subscription name if client sends, else set "Free Trial"
+if (!$SubscriptionName) {
+    $SubscriptionName = "Free Trial"
+}
 # connect to Azure
 Write-Output "Connecting to subscription"
-Connect-AzAccount  -Subscription "Free Trial"
-
+Connect-AzAccount  -Subscription $SubscriptionName
 #Creating Resource group
 Write-Output "Creating RG"
-New-AzResourceGroup -Name PoonamRG1 -Location "Australia East"
+New-AzResourceGroup -Name $resourceGroupName -Location "Australia East"
 
 #Deploy template
 Write-Output "creating template parameter object"
